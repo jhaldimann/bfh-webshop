844ac0cd5dfe7da48a1397b3ef542dd6ec431a97
Julian <julian@praxishaldimann.ch>
2019-09-18 14:16:19 +0200

    Add gitignore file and index file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>GameFameClothing</title>
+    </head>
+    <body>
+        <h1>Gamefameclothing</h1>
+        <?php phpinfo(); ?>
+    </body>
+</html>
+
+
