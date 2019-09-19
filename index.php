0ae27052627a0d94b97c903dff071f9d4e8ab3d2
Julian Haldimann <julian@praxishaldimann.ch>
2019-09-19 23:26:37 +0200

    Adapt structure and add header

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
         <title>GameFameClothing</title>
     </head>
     <body>
-        <h1>Gamefameclothing</h1>
-        <?php phpinfo(); ?>
+        <div class="header">
+            <img class="home-logo" src="images/home.png" alt="home">
+            <h2>GamefameClothing</h2>
+            <img class="user-logo" src="images/user.png" alt="user">
+            <img class="shopping-cart-logo" src="images/shoppingcart.png" alt="shoppingcart">
+        </div>
     </body>
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </html>
 
 
