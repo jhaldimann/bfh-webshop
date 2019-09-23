20fab11fe921d64df9455a8351a208bb66a266bc
Beastlike3773 <manuel.gasser@bfh.students.ch>
2019-09-23 16:00:54 +0200

    Added basic layout for product page

--- views/header.php
+++ views/header.php
@@ -1,7 +1,8 @@
 <?php
 echo "  <header class=\"header\">
-            <img class=\"home-logo\" src=\"./images/home.png\" alt=\"home\">
-            <h2>GamefameClothing</h2>
-            <img class=\"user-logo\" src=\"./images/user.png\" alt=\"user\">
-            <img class=\"shopping-cart-logo\" src=\"./images/shoppingcart.png\" alt=\"shoppingcart\">
+            <img class=\"home-logo\" src=\"/images/home.png\" alt=\"home\">
+            <h2>GameFameClothing</h2>
+            <img class=\"user-logo\" src=\"/images/user.png\" alt=\"user\">
+            <img class=\"shopping-cart-logo\" src=\"/images/shoppingcart.png\" alt=\"shoppingcart\">
+            <link rel=\"stylesheet\" href=\"/styles/header.css\">
         </header>";
--- views/product.php
+++ views/product.php
@@ -1,1 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>GameFameClothing</title>
+    </head>
+    <body>
+    <?php include './header.php'; ?>
+    <section class="product">
+        <img id="product-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrvn5FHHEUeWRTtLIG0SMjHbla700rHzxdpw-edXUKf6YAojxE" alt="product">
+        <section>
+            <h2 id="description-title">Description</h2>
+            <p id="product-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores ducimus eos odio perspiciatis placeat quidem reprehenderit similique soluta ullam. A ab dolorem et in iusto modi, pariatur rerum veritatis.</p>
+            <label id="size-selector-label" for="size-selector">Size:</label>
+            <select id="size-selector"></select>
+            <button id="add-to-cart">
+                <img id="add-to-cart-img" src="/images/shoppingcart.png" alt="add to cart">
+            </button>
+        </section>
+    </section>
+    </body>
+    <link href="/styles/product.css" rel="stylesheet">
+    <link href="/styles/global.css"
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</html>
