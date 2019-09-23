a2ca6d4fcd29636ac6abbbed35ffc24b5b63c176
Beastlike3773 <manuel.gasser@bfh.students.ch>
2019-09-23 19:24:49 +0200

    Changed add to cart button design and added zoom on hover on product image

---
+++ views/footer.php
@@ -0,0 +1,5 @@
+<?php
+echo "  <footer class=\"footer\">
+            <p id='copyright'>© GameFameClothing Inc.</p>
+            <link href='/styles/footer.css' rel='stylesheet'>
+        </footer>";
--- views/product.php
+++ views/product.php
@@ -8,18 +8,22 @@
     <?php include './header.php'; ?>
     <section class="product">
         <img id="product-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrvn5FHHEUeWRTtLIG0SMjHbla700rHzxdpw-edXUKf6YAojxE" alt="product">
-        <section>
+        <section id="information">
             <h2 id="description-title">Description</h2>
             <p id="product-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores ducimus eos odio perspiciatis placeat quidem reprehenderit similique soluta ullam. A ab dolorem et in iusto modi, pariatur rerum veritatis.</p>
             <label id="size-selector-label" for="size-selector">Size:</label>
-            <select id="size-selector"></select>
+            <select id="size-selector">
+                <option value="na">N/A</option>
+            </select>
             <button id="add-to-cart">
                 <img id="add-to-cart-img" src="/images/shoppingcart.png" alt="add to cart">
+                <label id="add-to-cart-label"l>Add to cart</label>
             </button>
         </section>
     </section>
+    <?php include './footer.php'; ?>
     </body>
     <link href="/styles/product.css" rel="stylesheet">
-    <link href="/styles/global.css"
+    <link href="/styles/global.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </html>
