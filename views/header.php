e6ad81261c7fe22b56c6b54d4c8373760f8f144f
Beastlike3773 <manuel.gasser@bfh.students.ch>
2019-09-24 08:38:39 +0200

    Changed add to cart button design, adjusted header location, added link to homepage on home icon.

--- views/header.php
+++ views/header.php
@@ -1,6 +1,8 @@
 <?php
 echo "  <header class=\"header\">
-            <img class=\"home-logo\" src=\"/images/home.png\" alt=\"home\">
+            <a href='/'>            
+                <img class=\"home-logo\" src=\"/images/home.png\" alt=\"home\">
+            </a>
             <h2>GameFameClothing</h2>
             <img class=\"user-logo\" src=\"/images/user.png\" alt=\"user\">
             <img class=\"shopping-cart-logo\" src=\"/images/shoppingcart.png\" alt=\"shoppingcart\">
