31c431e7dbea763de056a94624ee19e1afa18fbf
Julian Haldimann <julian@praxishaldimann.ch>
2019-09-24 13:02:24 +0200

    Add first elements to the products page

--- index.php
+++ index.php
@@ -9,27 +9,33 @@
 
         <section class="gender-section main-section">
             <h3>Select a category</h3> <br/>
-            <a href="views/products.php">
-                <div class="section"></div>
+            <a href="views/products.php?type=women">
+                <div class="section">
+                    <h3 class="section-title">Women</h3>
+                </div>
             </a>
-            <a href="views/products.php">
-                <div class="section"></div>
+            <a href="views/products.php?type=men">
+                <div class="section">
+                    <h3 class="section-title">Men</h3>
+                </div>
             </a>
-            <a href="views/products.php">
-                <div class="section"></div>
+            <a href="views/products.php?type=kids">
+                <div class="section">
+                    <h3 class="section-title">Kids</h3>
+                </div>
             </a>
         </section>
         <section class="special-offers main-section">
             <div class="title">
-                <img class="lightning" src="/images/lightning.png">
+                <img class="lightning" src="/images/lightning.png" alt="lightning">
                 <h2>Special Offers</h2>
-                <img class="lightning" src="/images/lightning.png">
+                <img class="lightning" src="/images/lightning.png" alt="lightning">
             </div>
         </section>
         <?php include 'views/footer.php'; ?>
     </body>
     <link rel="stylesheet" href="styles/header.css">
-    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/index.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </html>
 
--- views/products.php
+++ views/products.php
@@ -1,1 +1,20 @@
-<h1>Products</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Products</title>
+    </head>
+    <body>
+        <?php include './header.php'; ?>
+        <?php include './sidebar.php'; ?>
+
+        <section class="">
+            <?php echo "<h1>".$_GET['type']."</h1>"; ?>
+        </section>
+        <?php include './footer.php'; ?>
+    </body>
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</html>
+
